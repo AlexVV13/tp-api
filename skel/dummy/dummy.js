87c22ccda1ef3e3c18489f4e3f7cb69fbd80a535
Alex_VV <62617575+AlexVV13@users.noreply.github.com>
2020-12-27 11:16:08 +0100

    [-]Dropped node v10 & updated Dummy park

--- skel/dummy/dummy.js
+++ skel/dummy/dummy.js
@@ -1,134 +1,134 @@
-/**
-* When creating a new park, this is basically the base file for it
-* All is emptied, you just have to fill in the correct code & data for it to work :)
-*/
-// const moment = require('moment-timezone'); Some parks use timedata in their api's, sometimes you'll need moment-tz
-const fetch = require('node-fetch');
-const fs = require('fs');
+// Dummy park which is the base of all parks basically.
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+import fs from 'fs';
 
-require('dotenv').config();
-
-// Configure the variables
-const apiBase = process.env.DUMMYPARK_APIBASE;
-const apiKey = process.env.DUMMYPARK_APIKEY;
-
-// Dummy park options
-const options = {
-  name: 'Dummy',
-  timezone: 'Europe/Amsterdam',
-  latitude: 51.64990915659694,
-  longitude: 5.043561458587647,
-};
-
-// Load the Dummy poidata
-// const poidata = ('./data/parks/dummy/dummy_pois.json')
-// const poimock = ('./data/parks/dummy/dummy_poi_mock.json')
+import dotenv from 'dotenv';
+dotenv.config();
 
 /**
-* Get Dummy data
-* This data contains park data such as latlon
+* Dummy Park Object
 */
-async function getPark() {
-  return Promise.resolve(
-      options,
-  ).then(function(value) {
-    console.log(value);
-  });
-};
+export class Dummy {
+  /**
+   * Create a new Dummy Park object
+   * @param {object} options
+   */
+  constructor() {
+    this.name = 'Dummy';
 
-/**
-* Get All POIS of Dummy
-* This data contains all the POIS in Dummy
-*/
-async function getPOIS() {
-  return fetch(apiUrl,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: apiKey,
+    this.latutude = 48.266140769976715;
+    this.longitude = 7.722050520358709;
+
+    this.timezone = 'Europe/Amsterdam';
+
+    this.apiKey = process.env.DUMMY_API_KEY;
+    this.waitTimesURL = process.env.DUMMY_WAITTIMES_URL;
+
+    this.language = process.env.LANGUAGE;
+  }
+
+  // Load the Dummy poidata
+  // const poidata = ('./data/parks/dummy/dummy_pois.json');
+  // const poimock = ('./data/parks/dummy/dummy_poi_mock.json');
+
+  /**
+  * Get Dummy POI data
+  * This data contains general ride names, descriptions etc.
+  */
+  async getPOIS() {
+    return fetch(this.waitTimesURL,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: this.apiKey,
+          },
         },
-      },
-  )
-      .then((res) => res.json())
-      .then((rideData) => {
-        const poi = [];
-        rideData.forEach((ride) => {
-          // Do stuff with the ridedata
-          // POI Object with queues
-          const poiData = {
-            name: ride.name,
-            id: ride.shortId,
-            state: ride.state,
-            active: ride.active,
-            waitTime: ride.waitTime,
+    )
+        .then((res) => res.json())
+        .then((rideData) => {
+          const poi = {};
+          // Fetch POI DATA
+          // Poi Object
+          poi[id] = {
+            name: ride.fields.name,
+            id: id,
+            waitTime: null,
+            state: null,
+            active: null,
+            location: {
+              latitude: lat,
+              longitude: lon,
+            },
             meta: {
-              type: 'attraction',
+              single_rider: singlerider,
+              type: ride.fields.category,
+              area: ride.fields.empire,
             },
           };
-          poi.push(poiData);
+          fs.writeFile('./data/parks/efteling/efteling_pois.json', JSON.stringify(poi, null, 4), function(err) {
+            if (err) return console.log(err);
+          });
+          fs.writeFile('./data/parks/efteling/efteling_poi_mock.json', JSON.stringify(rideData.hits.hit, null, 4), function(err) {
+            if (err) return console.log(err);
+          });
+          return Promise.resolve(poi);
         });
-        fs.writeFile('./data/parks/dummy/dummy_pois.json', JSON.stringify(poi, null, 4), function(err) {
-          if (err) return console.log(err);
+  }
+
+  /**
+  * Get Dummy Park Hours data
+  * This data contains the hours data, used to display the operating hours of Dummy
+  */
+  async getOpHours() {
+    const currentYear = moment().format('YYYY');
+    const currentMonth = moment().format('MM');
+
+    return fetch(
+        this.waitTimesURL +
+        `${currentYear}/${currentMonth}`,
+        {
+          method: 'GET',
+        },
+    )
+        .then((res) => res.json())
+        .then((json) => {
+          const Calendar = [];
+          // Fetch Cal data
+          return Promise.resolve(Calendar);
         });
-        fs.writeFile('./data/parks/dummy/dummy_poi_mock.json', JSON.stringify(rideData, null, 4), function(err) {
-          if (err) return console.log(err);
-        });
-        return Promise.resolve(poi); // Although we saved them in a json file, we'll just return them for the queue attach function which will run next if you called getQueue()
-      });
+  }
+
+  /**
+  * Get All Dummy data in console
+  * This data contains all the data of Dummy, returned as json object
+  */
+  async getData() {
+    return await Promise.all([this.getQueue(), this.getOpHours()]).then((rides) => {
+      console.log(JSON.stringify(rides, null, 4));
+    });
+  }
+
+  /**
+  * Get Dummy Park Hours data in console
+  * This data contains the hours data, used to display the operating hours of Dummy
+  */
+  async getCalendar() {
+    return await this.getOpHours().then((calendar) => {
+      console.log(calendar);
+    });
+  }
+
+  /**
+  * Get Dummy Queuetimes data in console
+  * This data contains the queuetimes data, used to display the queuetimes of Dummy
+  */
+  async getWaitTime() {
+    return await this.getQueue().then((rides) => {
+      console.log(rides);
+    });
+  };
 }
 
-/**
-* Get All Operating Hours of Dummy
-* This data contains all the Operating Hours in Dummy
-*/
-async function getOpHours() {
-  return fetch(
-      apiBase,
-      {
-        method: 'GET',
-      },
-  )
-      .then((res) => res.json())
-      .then((json) => {
-        const Calendar = [];
-        // Execute Calendar stuff here
-        return Promise.resolve(Calendar);
-      });
-}
+export default Dummy;
 
-/**
-* Get All Data of Dummy
-* This data contains all Dummy's data, which is fetched earlier
-*/
-async function getData() {
-  return await Promise.all([getPOIS(), getOpHours()]).then((rides) => {
-    console.log(JSON.stringify(rides, null, 4));
-  });
-}
-
-/**
-* Get All Calendar Data of Dummy
-* This data contains all the calendar data of Dummy
-*/
-async function getCalendar() {
-  return await getOpHours().then((calendar) => {
-    console.log(calendar);
-  });
-}
-
-/**
-* Get All Queues of Dummy
-* This data contains all the Queues in Dummy, limited to their fast-lane services
-*/
-async function getWaitTime() {
-  return await getPOIS().then((rides) => {
-    console.log(rides);
-  });
-}
-
-module.exports.getPark = getPark;
-module.exports.getPOIS = getPOIS;
-module.exports.getOpHours = getOpHours;
-module.exports.getData = getData;
-module.exports.getCalendar = getCalendar;
-module.exports.getWaitTime = getWaitTime;
