1e85919b92ec53027b9a87d839bd689b0472c978
Alex_VV <62617575+AlexVV13@users.noreply.github.com>
2020-12-27 11:10:07 +0100

    [+]Walibi Holland is now compatible again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import all parks here
 import tpapi from './lib/parks/index.js';
 
-const park = new tpapi.parks.Efteling();
+const park = new tpapi.parks.WalibiHolland();
 
 // Fetch POIS
 park.getData(); // Get All data for efteling, you could also run getCalendar() or getWaitTime()
