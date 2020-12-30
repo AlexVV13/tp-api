014352b85ca0f4be1e411df0100e7c77e41f72ec
Alex_VV <62617575+AlexVV13@users.noreply.github.com>
2020-12-30 10:39:56 +0100

    [+]Redone POI fetching of Efteling

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import tpapi from '@alexvv13/tpapi';
 const park = new tpapi.parks.Efteling();
 
 // Fetch POIS Example usage of Europa-Park
-park.getWaitTime().then((rideTimes) => {
+park.buildMerchandisePOI().then((rideTimes) => {
   console.log(rideTimes);
   // rideTimes.forEach((ride) => {
-  //  console.log(`${ride.name}: ${ride.waitTime} minutes wait (${ride.state})`);
+  //  console.log(`${ride.meta.minHeight}: ${ride.waitTime} minutes wait (${ride.state})`);
   // });
 });
 
