c936ffb80c425c38ccbc91c08be4381c9edd782a
Alex_VV <62617575+AlexVV13@users.noreply.github.com>
2021-04-26 07:39:23 +0200

    [-] Removed testing stuff

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Import all parks here
 import tpapi from '@alexvv13/tpapi';
-import fs from 'file-system';
 
-const park = new tpapi.parks.Efteling();
+const park = new tpapi.parks.EuropaPark();
 
 // Print queues Example usage of Europa-Park
 park.getWaitTimes().then((rideTimes) => {
-  fs.writeFile('./rides.json', JSON.stringify(rideTimes));
-  //rideTimes.forEach((ride) => {
-  //  console.log(`${ride.name}: ${ride.queues.standBy.waitTime} minutes wait | ${ride.queues.standBy.status}`);
-  //})
+  rideTimes.forEach((ride) => {
+    console.log(`${ride.name}: ${ride.queues.standBy.waitTime} minutes wait | ${ride.queues.standBy.status}`);
+  })
 });
 
 // You can also call getCalendar(), getPark()
