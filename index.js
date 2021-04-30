695e24e76678cc32cb0a6326362b079160b5da25
AlexVV13 <alexvvught@outlook.com>
2021-04-30 19:52:18 +0200

    [~]Fixed index

--- index.js
+++ index.js
@@ -3,21 +3,11 @@ import tpapi from '@alexvv13/tpapi';
 
 const park = new tpapi.parks.EuropaPark();
 
-const parks = {};
-for (const park in tpapi.parks) {
-  parks[park] = new tpapi.parks[park]();
-}
-
-
-for (const park in parks) {
-  console.log(parks[park].Name)
-}
-
 // Print queues Example usage of Europa-Park
 park.getWaitTimes().then((rideTimes) => {
-  //rideTimes.forEach((ride) => {
-  //  console.log(`${ride.name}: ${ride.queues.standBy.waitTime} minutes wait | ${ride.queues.standBy.status}`);
-  //})
+  rideTimes.forEach((ride) => {
+    console.log(`${ride.name}: ${ride.queues.standBy.waitTime} minutes wait | ${ride.queues.standBy.status}`);
+  })
 });
 
 // You can also call getCalendar(), getPark()
