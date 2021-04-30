621aa6460e0334f8b2147a05b41957be8f0bc061
AlexVV13 <alexvvught@outlook.com>
2021-04-30 19:52:06 +0200

    [~]nothing

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Import all parks here
 import tpapi from '@alexvv13/tpapi';
-import fs from 'file-system';
 
-const park = new tpapi.parks.Efteling();
+const park = new tpapi.parks.EuropaPark();
+
+const parks = {};
+for (const park in tpapi.parks) {
+  parks[park] = new tpapi.parks[park]();
+}
+
+
+for (const park in parks) {
+  console.log(parks[park].Name)
+}
 
 // Print queues Example usage of Europa-Park
 park.getWaitTimes().then((rideTimes) => {
-  fs.writeFile('./rides.json', JSON.stringify(rideTimes));
   //rideTimes.forEach((ride) => {
   //  console.log(`${ride.name}: ${ride.queues.standBy.waitTime} minutes wait | ${ride.queues.standBy.status}`);
   //})
