43db79fc66d600fc0310a6f545ccf63a60ec5c96
AlexVV13 <alexvvught@outlook.com>
2021-05-03 19:20:16 +0200

    [~]Fix linting issues

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // Import all parks here
 import tpapi from '@alexvv13/tpapi';
 
-const park = new tpapi.parks.WalibiBelgium();
-
-park.getQueue().then((hours) => {
-  console.log(hours);
-});
+const park = new tpapi.parks.EuropaPark();
 
 // Print queues Example usage of Europa-Park
-/*park.getWaitTimes().then((rideTimes) => {
+park.getWaitTimes().then((rideTimes) => {
   rideTimes.forEach((ride) => {
     console.log(`${ride.name}: ${ride.queues.standBy.waitTime} minutes wait | ${ride.queues.standBy.status}`);
   })
-}); */
+});
 
 // You can also call getCalendar(), getPark()
